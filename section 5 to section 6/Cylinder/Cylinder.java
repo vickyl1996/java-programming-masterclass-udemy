563645143d4f5e4b6a9f14867e96129fe5f2f2a5
vickyl1996 <104000917+vickyl1996@users.noreply.github.com>
2022-09-13 18:16:48 +0530

    Add files via upload

---
+++ section 5 to section 6/Cylinder/Cylinder.java
@@ -0,0 +1,14 @@
+public class Cylinder extends Circle {
+    // write your code here
+    private double height;
+    public Cylinder(double radius, double height) {
+        super(radius);
+        this.height = height < 0 ? 0 : height;
+    }
+    public double getHeight() {
+        return height;
+    }
+    public double getVolume(){
+        return getArea() * getHeight();
+    }
+}
---
+++ section 5 to section 6/Pool Area/Cuboid.java
@@ -0,0 +1,14 @@
+public class Cuboid extends Rectangle {
+    // write your code here
+    private double height;
+    public Cuboid(double width, double length, double height) {
+        super(width, length);
+        this.height = height < 0 ? 0 : height;
+    }
+    public double getHeight() {
+        return height;
+    }
+    public double getVolume() {
+        return getArea() * this.height;
+    }
+}
